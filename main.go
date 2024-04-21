a9132d91acfad13ab872f8df01c4abf7b2e2e114
BilyHakim <bilyhakim1@gmail.com>
2024-04-21 13:46:44 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
